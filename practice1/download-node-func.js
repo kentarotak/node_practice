4eb5e0134a0a053cd0685854c8588b82073679a2
root <root@localhost.localdomain>
2017-03-20 08:02:12 +0900

    up

---
+++ practice1/download-node-func.js
@@ -0,0 +1,26 @@
+
+
+download(
+	"http://www.aozora.gr.jp/index_pages/person81.html",
+	"yazawakenji.html",
+        function(){ console.log("ok, kenji"); });
+
+download(
+        "http://www.aozora.gr.jp/index_pages/person148.html",
+        "natumesoseki.html",
+        function(){ console.log("ok, soseki"); });
+
+
+function download(url, savepath, callback) {
+        var http = require('http');
+        var fs = require('fs');
+        var outfile = fs.createWriteStream(savepath);
+        var req = http.get(url, function(res) {
+                res.pipe(outfile);
+                res.on('end', function() {
+                        outfile.close();
+                        callback();
+                });
+        });
+}
+
